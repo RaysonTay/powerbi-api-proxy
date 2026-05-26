8b93d8909092c17f758fe4367fc30313accd509c
TAY <raysonka-meng.tay@marlink.com>
2026-05-26 17:14:05 +0800

    added dotenv and caching

--- weather_miniproj/server.js
+++ weather_miniproj/server.js
@@ -1,4 +1,4 @@
-requre('dotenv').config();
+require('dotenv').config();
 
 const express = require('express');
 const axios = require('axios');
@@ -9,9 +9,28 @@ const PORT = process.env.PORT || 3000;
 const LATITUDE = process.env.LATITUDE;
 const LONGITUDE = process.env.LONGITUDE;
 
+let cachedData = null;
+let lastFetchTime = null;
+const CACHE_DURATION = 60 * 1000; // 60s
+
 app.get('/weather', async (req, res) => {
 
     try {
+        const now = Date.now();
+        
+        if (
+            cachedData &&
+            lastFetchTime &&
+            (now - lastFetchTime < CACHE_DURATION)
+        ) {
+
+            console.log('Serving cached data');
+
+            return res.json(cachedData);
+        }
+        
+        console.log('Fetching fresh data from API');
+
 
         const response = await axios.get(
             'https://api.open-meteo.com/v1/forecast',
@@ -32,11 +51,14 @@ app.get('/weather', async (req, res) => {
             humidity: hourly.relative_humidity_2m[index]
         }));
 
+        cachedData = transformedData;
+        lastFetchTime = now;
+
         res.json(transformedData);
 
     } catch (error) {
 
-        console.error(error.message);
+        console.error('API Error:', error.message);
 
         res.status(500).json({
             error: 'Failed to fetch weather data'
